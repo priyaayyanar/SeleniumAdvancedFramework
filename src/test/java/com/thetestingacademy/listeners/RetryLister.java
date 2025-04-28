a0b0ad530734f21f55c5a37903546ef9726f1f4a
Priya Ayyanar <priyamcanandha@gmail.com>
2025-04-28 17:37:51 +0530

    Page Factory, Log4j, RetryListener

--- src/test/java/com/thetestingacademy/listeners/RetryLister.java
+++ src/test/java/com/thetestingacademy/listeners/RetryLister.java
@@ -1,4 +1,14 @@
 package com.thetestingacademy.listeners;
 
-public class RetryLister {
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class RetryLister implements IAnnotationTransformer {
+    @Override
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod){
+        annotation.setRetryAnalyzer(RetryAnalyzer.class);
+    }
 }
--- src/test/java/com/thetestingacademy/tests/VWO_TestCases/testCase_PF/TestVWOLogin_PF.java
+++ src/test/java/com/thetestingacademy/tests/VWO_TestCases/testCase_PF/TestVWOLogin_PF.java
@@ -2,16 +2,25 @@ package com.thetestingacademy.tests.VWO_TestCases.testCase_PF;
 
 import com.thetestingacademy.base.CommonToAllTest;
 import com.thetestingacademy.driver.DriverManager;
+import com.thetestingacademy.listeners.RetryAnalyzer;
 import com.thetestingacademy.pages.pageFactory.LoginPage_PF;
 import com.thetestingacademy.utils.PropertiesReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
+@Test(retryAnalyzer = RetryAnalyzer.class)
 public class TestVWOLogin_PF extends CommonToAllTest {
+
+    private static final Logger logger = LogManager.getLogger(TestVWOLogin_PF.class);
 
     @Test
     public void testLoginNegativeVWO_PF(){
+
+        logger.info("Starting the TestCases PageFactory");
 
         WebDriver driver  = DriverManager.getDriver();
         driver.get(PropertiesReader.readKey("url"));
@@ -21,6 +30,7 @@ public class TestVWOLogin_PF extends CommonToAllTest {
 
         Assert.assertEquals(error_msg, PropertiesReader.readKey("error_message"));
 
+        logger.info("End of the TestCases PageFactory");
     }
 
 }
